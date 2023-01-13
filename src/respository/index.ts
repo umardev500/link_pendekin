a0e7a3c918c36a884f6b6ebc0d0af125da5bb8bd
umar <umar.dev500@gmail.com>
2023-01-14 01:37:45 +0700

    add auth header on post link

--- src/respository/index.ts
+++ src/respository/index.ts
@@ -1,1 +1,2 @@
 export * from './LinkRepo'
+export * from './token'
--- src/store/features/linkSlice.ts
+++ src/store/features/linkSlice.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { getToken } from '../../respository'
 import { linkResponse, Payload } from '../../types'
 
 const initialState: linkResponse = {
@@ -13,13 +14,24 @@ export const createPostLink = createAsyncThunk('link/createLink', async ({ url, 
         short,
     }
 
+    let headers: any = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    }
+
+    const token = getToken()
+    if (token !== null) {
+        headers = {
+            ...headers,
+            Authorization:
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1lIjoidW1hciIsImVtYWlsIjoidW1hci5kZXY1MDBAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmIkMTAkaFhXSWZtNVZELmNmNnkwVEdzbk5kTzdFUWtsVjJ1aVdMbHpVcFRrNkJyZWVrZ250a0tCLmEiLCJjcmVhdGVkQXQiOiIyMDIzLTAxLTEzVDE3OjIwOjIyLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIzLTAxLTEzVDE3OjIwOjIyLjAwMFoifSwiaWF0IjoxNjczNjMzMTc5LCJleHAiOjE2NzM4MDU5Nzl9.keJAbctnAqqk-bYBmMtgC2_SmbRK2jvYTVMM08PF7xw',
+        }
+    }
+
     try {
         const response = await fetch('http://localhost:2000/links', {
             method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
+            headers,
             body: JSON.stringify(requestBody),
         })
 
